81865c5b4a2f80b7fe240fdf1c910269f3140106
dylanjcassidy <dylan.j.cassidy@uconn.edu>
2024-06-30 23:06:15 -0400

    Fixed high score bug

--- source/repos/Sproutfall/Sproutfall/UIHighscoreView.cpp
+++ source/repos/Sproutfall/Sproutfall/UIHighscoreView.cpp
@@ -12,7 +12,21 @@ UIHighscoreView::UIHighscoreView(sf::Texture* atlasTexture, sf::Vector2f positio
 			m_Highscores[i][j]->setTexture(*m_atlasTexture);
 			m_Highscores[i][j]->setOrigin(16, 16);
 			m_Highscores[i][j]->setScale(2, 2);
-			m_Highscores[i][j]->setTextureRect(m_ColonCoords);
+			if (j != 3 && j != 4 && j != 6 && j != 9)
+			{
+				m_Highscores[i][j]->setTextureRect(m_DashCoords);
+			}
+			else
+			{
+				if (j != 4)
+				{
+					m_Highscores[i][j]->setTextureRect(m_ColonCoords);
+				}
+				else
+				{
+					m_Highscores[i][j]->setTextureRect(sf::IntRect(0, 0, 1, 1));
+				}
+			}
 		}
 	}
 	m_UIPosition = position;
@@ -35,44 +49,48 @@ void UIHighscoreView::ParseScores()
 {
 	for (int i = 0; i < 5; i++)
 	{
-		string name = m_HighscoreManager->getName(i);
-		for (int j = 0; j < 3; j++)
+		if (m_HighscoreManager->getPopulated(i))
 		{
-			if (j < name.size())
+			string name = m_HighscoreManager->getName(i);
+			for (int j = 0; j < 3; j++)
 			{
-				m_Highscores[i][j]->setTextureRect(m_Letters[toupper(name[j]) - 'A']);
+				if (j < name.size())
+				{
+					m_Highscores[i][j]->setTextureRect(m_Letters[toupper(name[j]) - 'A']);
+				}
+				else
+				{
+					m_Highscores[i][j]->setTextureRect(m_DashCoords);
+				}
+			}
+			m_Highscores[i][3]->setTextureRect(m_ColonCoords);
+			m_Highscores[i][4]->setTextureRect(sf::IntRect(0, 0, 1, 1));
+			int hours = m_HighscoreManager->getMins(i) / 60;
+			if (hours >= 10)
+			{
+				m_Highscores[i][5]->setTextureRect(m_Letters['S' - 'A']);
+				m_Highscores[i][6]->setTextureRect(m_Letters['L' - 'A']);
+				m_Highscores[i][7]->setTextureRect(m_Letters['O' - 'A']);
+				m_Highscores[i][8]->setTextureRect(m_Letters['W' - 'A']);
+				for (int j = 9; j < 12; j++)
+				{
+					m_Highscores[i][j]->setTextureRect(sf::IntRect(0, 0, 1, 1));
+				}
 			}
 			else
 			{
-				m_Highscores[i][j]->setTextureRect(m_DashCoords);
-			}
-		}
-		m_Highscores[i][3]->setTextureRect(m_ColonCoords);
-		m_Highscores[i][4]->setTextureRect(sf::IntRect(0, 0,1, 1));
-		int hours = m_HighscoreManager->getMins(i) / 60;
-		if (hours >= 10)
-		{
-			m_Highscores[i][5]->setTextureRect(m_Letters['S' - 'A']);
-			m_Highscores[i][6]->setTextureRect(m_Letters['L' - 'A']);
-			m_Highscores[i][7]->setTextureRect(m_Letters['O' - 'A']);
-			m_Highscores[i][8]->setTextureRect(m_Letters['W' - 'A']);
-			for (int j = 9; j < 12; j++)
-			{
-				m_Highscores[i][j]->setTextureRect(sf::IntRect(0, 0, 1, 1));
-			}
-		}
-		else
-		{
-			m_Highscores[i][5]->setTextureRect(m_Numbers[hours]);
-			m_Highscores[i][6]->setTextureRect(m_ColonCoords);
-			int mins = m_HighscoreManager->getMins(i);
-			m_Highscores[i][7]->setTextureRect(m_Numbers[mins / 10]);
-			m_Highscores[i][8]->setTextureRect(m_Numbers[mins % 10]);
-			m_Highscores[i][9]->setTextureRect(m_ColonCoords);
-			int secs = m_HighscoreManager->getSecs(i);
-			m_Highscores[i][10]->setTextureRect(m_Numbers[secs / 10]);
-			m_Highscores[i][11]->setTextureRect(m_Numbers[secs % 10]);
+				m_Highscores[i][5]->setTextureRect(m_Numbers[hours]);
+				m_Highscores[i][6]->setTextureRect(m_ColonCoords);
+				int mins = m_HighscoreManager->getMins(i);
+				mins -= (hours * 60);
+				m_Highscores[i][7]->setTextureRect(m_Numbers[mins / 10]);
+				m_Highscores[i][8]->setTextureRect(m_Numbers[mins % 10]);
+				m_Highscores[i][9]->setTextureRect(m_ColonCoords);
+				int secs = m_HighscoreManager->getSecs(i);
+				m_Highscores[i][10]->setTextureRect(m_Numbers[secs / 10]);
+				m_Highscores[i][11]->setTextureRect(m_Numbers[secs % 10]);
 
+			}
 		}
 	}
 }
